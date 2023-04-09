88360ddf322b670ae6a762fd6820d661c6f6b29f
Mike Levin <miklevin@gmail.com>
2023-04-09 10:49:49 -0400

    Way better use of CONSTANTS

--- chopchop.py
+++ chopchop.py
@@ -14,29 +14,38 @@ from sqlitedict import SqliteDict as sqldict
 # Define command line arguments
 aparser = argparse.ArgumentParser()
 add_arg = aparser.add_argument
-add_arg("-r", "--repo", required=True)
-add_arg("-f", "--file", default="journal.md")
+
+add_arg("-f", "--full_path", required=True)
 add_arg("-a", "--author", default="Mike Levin")
 add_arg("-b", "--blog", default="blog")
-add_arg("-p", "--path", default="/home/ubuntu/repos/hide/")
 add_arg("-o", "--output", default="_posts")
 
-# Parse command line arguments
+# Parse command line args as CONSTANTS
 args = aparser.parse_args()
-author = args.author
-output = args.output
-repo = args.repo
-file = args.file
-blog = args.blog
-path = args.path
+BLOG = args.blog
+OUTPUT = args.output
+AUTHOR = args.author
+FULL_PATH = args.full_path
+
+# Parse full path into path, repo, and file
+parts = FULL_PATH.split("/")
+FILE = parts[-1]
+REPO = parts[-2] + "/"
+PATH = "/".join(parts[:-2]) + "/"
+print(f"FULL_PATH: {FULL_PATH}")
+print(f"PATH: {PATH}")
+print(f"REPO: {REPO}")
+print(f"FILE: {FILE}")
+
+raise SystemExit(0)
 
 # Define Constants
 SUMMARY_LENGTH = 350
 PARSE_TOKEN = "\n" + "-"*80 + "\n"
-OUTPUT_PATH = f"{path}{repo}/{output}"
-FULL_FILE = f"{path}{repo}/{file}"
-REPO_DATA = f"{path}{repo}/_data/"
-print(f"Processing {FULL_FILE}")
+OUTPUT_PATH = f"{PATH}{REPO}{OUTPUT}"
+
+REPO_DATA = f"{PATH}{REPO}_data/"
+print(f"Processing {FULL_PATH}")
 
 # Create output path if it doesn't exist
 Path(OUTPUT_PATH).mkdir(parents=True, exist_ok=True)
@@ -51,9 +60,9 @@ with open ("openai.txt") as fh:
     openai.api_key = fh.readline()
 
 
-def parse_journal(FULL_FILE):
+def parse_journal(FULL_PATH):
     """Parse a journal file into posts. Returns a generator of posts."""
-    with open(FULL_FILE, "r") as f:
+    with open(FULL_PATH, "r") as f:
         post_str = f.read()
         posts = post_str.split(PARSE_TOKEN)
         for post in posts:
@@ -93,7 +102,7 @@ def write_post_to_file(post, index):
             slug = slugify(title.replace("'", ""))
             top_matter.append(f"title: {title}")
             top_matter.append(f"slug: {slug}")
-            top_matter.append(f"permalink: /{blog}/{slug}/")
+            top_matter.append(f"permalink: /{BLOG}/{slug}/")
         else:
             # Subsequent lines are either top matter or content
             if not line:
@@ -131,7 +140,7 @@ def write_post_to_file(post, index):
         summary.replace('"', "'")
         top_matter.append(f"description: {summary}")
     top_matter.append(f"layout: post")
-    top_matter.append(f"author: {author}")
+    top_matter.append(f"author: {AUTHOR}")
     top_matter.append("---")
     top_matter.append("")
     top_matter.extend(content)
@@ -146,7 +155,7 @@ def write_post_to_file(post, index):
     fdate = date_str.strftime("%m/%d/%Y")
     if len(summary) > SUMMARY_LENGTH:
         summary = summary[:SUMMARY_LENGTH] + "..."
-    link = f"- [{title}](/{blog}/{slug}/) {fdate}<br/>\n  {summary}"
+    link = f"- [{title}](/{BLOG}/{slug}/) {fdate}<br/>\n  {summary}"
     return link
 
 
@@ -186,7 +195,7 @@ def summarize(text):
 
 
 # Parse the journal file
-posts = parse_journal(FULL_FILE)
+posts = parse_journal(FULL_PATH)
 links = []
 for i, post in enumerate(posts):
     link = write_post_to_file(post, i)
@@ -195,5 +204,5 @@ for i, post in enumerate(posts):
 
 # Write index page
 index_page = "\n".join(links)
-with open(f"{path}{repo}/_includes/posts-main.html", "w", encoding="utf-8") as fh:
+with open(f"{PATH}{REPO}_includes/posts-main.html", "w", encoding="utf-8") as fh:
     fh.writelines(index_page)
